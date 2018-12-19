0abf1a16f93bc40a9fb897d6bf22030802cf6673
Tim <talbert1209@gmail.com>
2018-12-18 18:59:04 -0600

    Working solution.

--- HeroMonsterClass Part2/Default.aspx.cs
+++ HeroMonsterClass Part2/Default.aspx.cs
@@ -11,22 +11,58 @@ namespace HeroMonsterClasses_Part2
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Character hero = new Character(){AttackBonus = true, DamageMaximum = 15, Health = 100, Name = "Tim"};
-            Character monster = new Character() {AttackBonus = false, DamageMaximum = 17, Health = 100, Name = "Phoebe"};
+            Dice die = new Dice(){Sides = 2};
 
-            monster.Defend(hero.Attack());
-            hero.Defend(monster.Attack());
+            Character hero = new Character(){AttackBonus = false, DamageMaximum = 15, Health = 100, Name = "Tim"};
+            Character monster = new Character() {AttackBonus = false, DamageMaximum = 14, Health = 100, Name = "Phoebe"};
 
-            DisplayStats(hero);
-            DisplayStats(monster);
+            //decide which character has the attack bonus and attack
+            if (die.Roll() == 0)
+            {
+                hero.AttackBonus = true;
+                monster.Defend(hero.Attack(die));
+            }
+            else
+            {
+                monster.AttackBonus = true;
+                hero.Defend(monster.Attack(die));
+            }
+
+            bool winner = false;
+            while (!winner)
+            {
+                monster.Defend(hero.Attack(die));
+                hero.Defend(monster.Attack(die));
+
+                DisplayStats(hero);
+                DisplayStats(monster);
+
+                if (hero.Health <= 0 || monster.Health <= 0)
+                {
+                    winner = true;
+                    if (hero.Health > monster.Health)
+                        Label1.Text += $"{hero.Name} is the winner!";
+                    else
+                        Label1.Text += $"{monster.Name} is the winner!";
+                }
+            }
+        }
+
+        
+        public void UseBonusAttack(Character character, Dice die)
+        {
+            if (character.AttackBonus)
+            {
+                character.Attack(die);
+            }
         }
 
         public void DisplayStats(Character character)
         {
-            Label1.Text += $"<br/>Name: {character.Name}, " +
+            Label1.Text += $"<p>Name: {character.Name}, " +
                            $"Attack Bonus: {character.AttackBonus}, " +
                            $"DamageMaximum: {character.DamageMaximum}, " +
-                           $"Health: {character.Health}";
+                           $"Health: {character.Health}<p/>";
         }
         public class Character
         {
@@ -35,15 +71,26 @@ namespace HeroMonsterClasses_Part2
             public int DamageMaximum { get; set; }
             public bool AttackBonus { get; set; }
 
-            public int Attack()
+            public int Attack(Dice die)
             {
-                Random random = new Random();
-                return random.Next(0,DamageMaximum + 1);
+                die.Sides = DamageMaximum;
+                return die.Roll();
             }
 
             public void Defend(int damage)
             {
                 Health -= damage;
+            }
+        }
+
+        public class Dice
+        {
+            public int Sides { get; set; }
+            readonly Random random = new Random();
+
+            public int Roll()
+            {
+                return random.Next(Sides);
             }
         }
     }
